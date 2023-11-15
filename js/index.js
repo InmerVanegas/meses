44deff0e7d9d8d326cdea977cd0d03533c4b3d03
InmerVanegas <inmer.vanegas@shopstack.io>
2023-11-15 10:33:25 -0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+document.getElementById("mostrarTarjeta1").addEventListener("click", function () {
+    document.getElementById("tarjeta1").style.display = "block";
+    document.getElementById("mostrarTarjeta2").style.display = "block";
+});
+
+document.getElementById("mostrarTarjeta2").addEventListener("click", function () {
+    document.getElementById("tarjeta2").style.display = "block";
+    document.getElementById("mostrarTarjeta3").style.display = "block";
+});
+
+document.getElementById("mostrarTarjeta3").addEventListener("click", function () {
+    document.getElementById("tarjeta3").style.display = "block";
+    document.getElementById("mostrarTarjeta4").style.display = "block";
+});
+
+document.getElementById("mostrarTarjeta4").addEventListener("click", function () {
+    document.getElementById("tarjeta4").style.display = "block";
+    document.getElementById("mostrarTarjeta5").style.display = "block";
+});
+
+document.getElementById("mostrarTarjeta5").addEventListener("click", function () {
+    document.getElementById("tarjeta5").style.display = "block";
+});
